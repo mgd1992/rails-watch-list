5e63250697d12ae03684df63b476a97abf013bd7
marco <marcoogarbini@gmail.com>
2024-08-17 19:58:35 +0200

    watch list almost finish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,6 +2,7 @@ class ListsController < ApplicationController
   before_action :set_list, only: %i[show]
   def index
     @lists = List.all
+    puts @lists.map(&:photo).inspect
   end
 
   def show
@@ -25,9 +26,10 @@ class ListsController < ApplicationController
 
   def set_list
     @list = List.find(params[:id])
+    raise ActiveRecord::RecordNotFound if @list.nil?
   end
 
   def strong_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
